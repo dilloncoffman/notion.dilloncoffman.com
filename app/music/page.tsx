63b6a0845fb733dcf78ec8ace6b9b661ae029de4
Dillon Coffman <dilloncoffman@Dillons-Mini.lan>
2023-06-24 12:16:14 -0400

    Installed Tailwind prettier plugin; Added favicon, title, description & pages

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Music() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home</h1>
+      <h1>Music</h1>
     </main>
   )
 }
