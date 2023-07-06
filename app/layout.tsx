3cccc622d13d95ce3eb69c9a34b8aac0acb4f8e1
Dillon Coffman <dillonscoffman@gmail.com>
2023-07-06 14:00:22 -0400

    Added openGraph image to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,9 @@ const quattrocento = Quattrocento_Sans({
 
 export const metadata = {
   title: "Dillon Coffman",
+  openGraph: {
+    images: ["/images/dillon-memeoji.png"],
+  },
   description:
     "Dillon Coffman is a developer who loves to push the limit and work with cutting-edge technology. A few years out of university, Dillon is excited to work alongside others to build apps that make a difference. In his free time he enjoys playing guitar and reading about everything from how to bootstrap a startup business to ancient philosophy. ~ Totally Real 3rd Person",
 }
