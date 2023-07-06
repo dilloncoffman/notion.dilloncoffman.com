b7bf36b64dd469f4b4868d982de87c5b092416b2
Dillon Coffman <dillonscoffman@gmail.com>
2023-07-05 21:14:56 -0400

    Refactored /music to get all YouTube videos while there's still a nextPageToken; Removed YouTubePagination component & searchParams from page props

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,46 +1,41 @@
-import { redirect } from "next/navigation"
 import { YouTubeVideoItem } from "../lib/interfaces/YouTubeVideoItem"
 import { YouTubeCard } from "./YouTubeCard"
-import YouTubePagination from "./YouTubePagination"
 
 export const metadata = {
   title: "Music | Dillon Coffman",
 }
 
-async function getYouTubeVideos(pageToken: string | string[]) {
-  const playlistItemsUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=48${
-    pageToken ? `&pageToken=${pageToken}` : ""
-  }&playlistId=${process.env.YOUTUBE_CHANNEL_PLAYLIST_ID}&key=${
-    process.env.YOUTUBE_API_KEY
-  }`
+async function getYouTubeVideos() {
+  let videos = []
+  let data
 
-  const res = await fetch(playlistItemsUrl, { cache: "force-cache" })
-  const data = res.json()
+  try {
+    let res = await fetch(
+      `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=48&playlistId=${process.env.YOUTUBE_CHANNEL_PLAYLIST_ID}&key=${process.env.YOUTUBE_API_KEY}`
+    )
+    data = await res.json()
 
-  return data
+    if (!!data) {
+      videos.push(...data?.items)
+      while (data?.nextPageToken) {
+        let nextPageToken = data?.nextPageToken
+        res = await fetch(
+          `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=48&pageToken=${nextPageToken}&playlistId=${process.env.YOUTUBE_CHANNEL_PLAYLIST_ID}&key=${process.env.YOUTUBE_API_KEY}`
+        )
+        data = await res.json()
+        videos.push(...data?.items)
+      }
+    }
+  } catch (e) {
+    console.error(e)
+  }
+
+  return { videos, totalResults: data?.pageInfo?.totalResults }
 }
 
-// This part is important! See https://github.com/vercel/next.js/discussions/47227
-export const dynamic = "force-dynamic"
-
-export default async function Music({
-  searchParams,
-}: {
-  searchParams?: { [key: string]: string | string[] | undefined }
-}) {
-  const pageToken = searchParams?.pageToken ?? ""
-  const youtubeData = await getYouTubeVideos(pageToken)
-  const {
-    items: youtubeVideos,
-    pageInfo,
-    prevPageToken,
-    nextPageToken,
-  } = youtubeData || {}
-
-  // https://rb.gy/oc650 - there's an issue with YouTube's API and pageToken sometimes returning empty items, redirect back to /music if this is the case
-  if (pageToken && youtubeVideos?.length <= 0) {
-    redirect("/music")
-  }
+export default async function Music() {
+  const { videos: youtubeVideos, totalResults } =
+    (await getYouTubeVideos()) || {}
 
   return (
     <div className="mx-auto max-w-4xl px-2">
@@ -49,20 +44,12 @@ export default async function Music({
       </h1>
       {youtubeVideos?.length > 0 && (
         <>
-          <p className="mb-2 text-xl">{pageInfo?.totalResults} videos</p>
-          <YouTubePagination
-            prevPageToken={prevPageToken}
-            nextPageToken={nextPageToken}
-          />
+          <p className="mb-2 text-xl">{totalResults} videos</p>
           <div className="my-2 grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-6 md:mb-4 md:grid-cols-3">
             {youtubeVideos.map((video: YouTubeVideoItem) => {
               return <YouTubeCard key={video.id} video={video} />
             })}
           </div>
-          <YouTubePagination
-            prevPageToken={prevPageToken}
-            nextPageToken={nextPageToken}
-          />
         </>
       )}
     </div>
