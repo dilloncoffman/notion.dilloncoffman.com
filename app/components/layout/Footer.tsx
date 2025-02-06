72de1bf3840e0fb835faec809b1eaa6a55cac21f
Dillon Coffman <dillonscoffman@gmail.com>
2025-02-05 20:36:06 -0500

    Updated Footer with correct range of copyright years

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "../Link"
 export default function Footer() {
   return (
     <footer className="text-md mb-4 text-center sm:text-xl">
-      <p>dillon coffman &copy; {new Date().getFullYear()}</p>
+      <p>dillon coffman &copy; 2016 - {new Date().getFullYear()}</p>
       <p>
         Proudly published with{" "}
         <Link href="https://vercel.com/" noopener>
